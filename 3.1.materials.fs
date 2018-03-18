247d84de9791fa6b2992f280f7a947465fd39c11
PBS <sortsimilar@gmail.com>
2018-03-18 17:19:30 -0400

    material, change light via time;

---
+++ 3.1.materials.fs
@@ -0,0 +1,45 @@
+#version 330 core
+out vec4 FragColor;
+
+struct Material { // 当描述一个物体的时候，我们可以用这三个分量来定义一个材质颜色; 我们为每个冯氏光照模型的分量都定义一个颜色向量。
+    vec3 ambient; // ambient材质向量定义了在环境光照下这个物体反射得是什么颜色，通常这是和物体颜色相同的颜色。
+    vec3 diffuse; // diffuse材质向量定义了在漫反射光照下物体的颜色。（和环境光照一样）漫反射颜色也要设置为我们需要的物体颜色。
+    vec3 specular; // specular材质向量设置的是镜面光照对物体的颜色影响（或者甚至可能反射一个物体特定的镜面高光颜色）。   
+    float shininess; // 反光度(Shininess);
+}; 
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 FragPos;  
+in vec3 Normal;  
+  
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+void main()
+{
+    // ambient 环境光
+    vec3 ambient = light.ambient * material.ambient;
+  	
+    // diffuse 漫反射
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    
+    // specular 镜面光
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);  
+        
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+} 
